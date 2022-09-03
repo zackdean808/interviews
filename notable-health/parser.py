b90dbbebb0b0b407ffaefc67101e4d615f089316
z <zack.dean@gmail.com>
2022-09-02 20:14:35 -0400

    Intial commit + algo

---
+++ notable-health/parser.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+
+import os
+import sys
+import argparse
+import re 
+
+
+
+
+
+
+if '__name__' == '__main__':
+    #main goes here 
+    # 1. read in file 
+    # 2. parse for text "number [1-9]" capture until '.' 
+    #   2a. Insert in list at that point 
+    # 3. parse for text "number next" capture until '.'
+    #   3a. Append line to list  
+    # I know I should put this else where but it's a quick note
+
+
+
+    
