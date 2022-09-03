31abb5cbf619d8b718db4d61beeaef18a771324f
z <zack.dean@gmail.com>
2022-09-02 20:56:54 -0400

    Found a solution to the wonky line breaks

--- notable-health/parser.py
+++ notable-health/parser.py
@@ -13,6 +13,24 @@ def append_number_n(numbered_list):
 def append_number_next(numbered_list):
     return 0 
 
+def clean_up():
+    os.remove("cleaned")
+
+def prep_file_for_splitting():
+    # open file for reading 
+    with open(sys.argv[1], 'r') as file:
+        fd = file.read()
+
+    # since I don't know where the line breaks are, I need to conver them to spaces
+    fd = fd.replace('\n', ' ')
+
+    # write cleaned file for splitting 
+    with open('cleaned', 'w') as file:
+        file.write(fd) 
+    
+    print("File written")
+
+
 
 if __name__ == "__main__":
     #main goes here 
@@ -26,12 +44,21 @@ if __name__ == "__main__":
     re_number_n = re.compile('Number (one|two|three|four|five|six|seven|eight|nine)\.$', re.IGNORECASE)
     re_number_next = re.compile('Number next\.$', re.IGNORECASE)
     re_split_text = re.compile('/.+?\./')
-
     split_text = []
     final_list = [] 
 
-    with open(sys.argv[1], 'r') as file:
-        lines = file.read()
+    # send remove newline characters
+    prep_file_for_splitting()
 
-        
-        
+    # split file into lines on the period 
+    
+
+
+
+
+
+    # clean up 
+    clean_up()
+
+
+
