8c0f7bfbc61730b3e7ec19609d99b48f3f6de034
z <zack.dean@gmail.com>
2022-09-02 20:38:59 -0400

    Restructing algo, plus cleanup

--- notable-health/parser.py
+++ notable-health/parser.py
@@ -17,24 +17,21 @@ def append_number_next(numbered_list):
 if __name__ == "__main__":
     #main goes here 
     # 1. read in file 
-    # 2. parse for text "number [1-9]" capture until '.' 
-    #   2a. Insert in list at that point 
-    # 3. parse for text "number next" capture until '.'
-    #   3a. Append line to list  
-    # I know I should put this else where but it's a quick note
+    # 2. split text from start to '.' append to list 
+    # 3. parse item in list
+    # 4. make decision on time in list 
+
 
     # ignoring case seems wise. 
     re_number_n = re.compile('Number (one|two|three|four|five|six|seven|eight|nine)\.$', re.IGNORECASE)
     re_number_next = re.compile('Number next\.$', re.IGNORECASE)
+    re_split_text = re.compile('/.+?\./')
 
+    split_text = []
+    final_list = [] 
 
     with open(sys.argv[1], 'r') as file:
-        lines = file.readlines()
+        lines = file.read()
 
-        for l in lines:
-            if re_number_n.match(l):
-                print ("number n")
-            if re_number_next.match(l):
-                print ("number next")
-            else:
-                print ("no match")
+        
+        
